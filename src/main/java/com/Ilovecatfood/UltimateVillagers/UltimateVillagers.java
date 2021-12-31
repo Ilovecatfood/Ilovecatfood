dbe28831d02d420cc9adacd42a02e3316b778f5b
Ilovecatfood <jwhitefoot42rk@gmail.com>
2021-12-31 09:28:45 +0000

    My mod

--- src/main/java/com/Ilovecatfood/UltimateVillagers/UltimateVillagers.java
+++ src/main/java/com/Ilovecatfood/UltimateVillagers/UltimateVillagers.java
@@ -13,18 +13,17 @@ import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fmlserverevents.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.server.command.ModIdArgument;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("ultimate_villagers")
+@Mod("assets/ultimate_villagers")
 public class UltimateVillagers
 
 {
-    public static final String MODID = "ultimate_villagers";
+    public static final String MODID = "assets/ultimate_villagers";
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
